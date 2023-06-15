cf5f0a652a38bc3e2ae2afaff5b77c04d06190aa
Zaryab Khalid <zaryabkhalid43@gmail.com>
2023-06-16 04:24:48 +0500

    updated ErrorHandler.js and added logs folder

--- middlewares/ErrorHandler.js
+++ middlewares/ErrorHandler.js
@@ -2,7 +2,7 @@ import { logEvents } from "./logger";
 function errorHandler(err, req, res, next) {
 	const statusCode = err.statusCode || 500;
 	const message = err.message || "Internal Server Error";
-	logEvents(`${statusCode}\t${message}`, "reqErrorLogs.log");
+	logEvents(`${statusCode}\t${message}\t${req.path}`, "reqErrorLogs.log");
 	res.status(statusCode).send({
 		error: {
 			statusCode: statusCode,
